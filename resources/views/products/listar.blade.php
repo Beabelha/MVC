b8f6f42c36d238fc3f277e8a9ec088a52da115ca
Beabelha <bea21gomes@gmail.com>
2020-06-17 19:13:23 -0300

    Crud feito para o exercicio da Aula 10

---
+++ resources/views/products/listar.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <h2 class="text-center"> Listagem de produtos </h2>
+    <br>
+    <div class="row justify-content-center">
+
+        <!-- Tabela de listagem de produtos -->
+        <table>
+            <tr>
+                <th> Nome |</th>
+                <th> Descrição |</th>
+                <th> Preço </th>
+            </tr>
+            @foreach ($produtos as $produto)
+            <tr>
+                <td>
+                    {{ $produto->name }}
+                </td>
+
+                <td>
+                    {{$produto->description}}
+                </td>
+
+                <td>
+                    {{$produto->price}}
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
+
+
+@endsection
